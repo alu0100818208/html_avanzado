14eea13a3041024a32000bdea18c97a42ee4df43
alu0100818208 <alu0100818208@ull.edu.es>
2017-03-15 19:10:03 +0000

    Corrección 3 index.js

--- trumbowyg/js/index.js
+++ trumbowyg/js/index.js
@@ -1,16 +1,17 @@
 $( document ).ready( function() {
-  $('.editor').trumbowyg({
-    svgPath: '/html_avanzado/trumbowyg/icons/icons.svg'
-    lang: 'es', // Idioma (es.min.js incluido)
-    btns: [
-            ['bold', 'italic', 'underline', 'strikethrough'],
-            ['superscript', 'subscript'],
-            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-            ['unorderedList', 'orderedList'],
-            ['link', 'insertImage'],
-            ['horizontalRule', 'removeformat'],
-            ['viewHTML'],
-            ['fullscreen']
-          ],
-  });}
+    $('.editor').trumbowyg({
+      svgPath: '/html_avanzado/trumbowyg/icons/icons.svg'
+      lang: 'es', // Idioma (es.min.js incluido)
+      btns: [
+              ['bold', 'italic', 'underline', 'strikethrough'],
+              ['superscript', 'subscript'],
+              ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+              ['unorderedList', 'orderedList'],
+              ['link', 'insertImage'],
+              ['horizontalRule', 'removeformat'],
+              ['viewHTML'],
+              ['fullscreen']
+            ],
+    });
+  });
 );
