de2b3580dc109ee1100b3d11e1b0bc8e235993fe
alu0100818208 <alu0100818208@ull.edu.es>
2017-03-17 16:47:47 +0000

    Photo Sphere Viewer

---
+++ Photo_Sphere_Viewer/js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+  var PSV = PhotoSphereViewer({
+    panorama:  'img/aviones.jps',
+    container: 'photosphere',
+    loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+  });
+
+});
