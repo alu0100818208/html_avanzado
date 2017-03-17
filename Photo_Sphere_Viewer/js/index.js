cfc99abc2f20cbb908d813eb7639969fd60e301e
alu0100818208 <alu0100818208@ull.edu.es>
2017-03-17 17:19:34 +0000

    imagen chachi

--- Photo_Sphere_Viewer/js/index.js
+++ Photo_Sphere_Viewer/js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
   var PSV = PhotoSphereViewer({
-    panorama:  'img/aviones.jps',
+    panorama:  'https://alu0100818208.github.io/html_avanzado/Photo_Sphere_Viewer/img/aviones.jpg',
     container: 'photosphere',
     loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
   });
